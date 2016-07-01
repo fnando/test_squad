2c8ee08e2370bb5061d48cb5317b726818706d2d
Nando Vieira <fnando.vieira@gmail.com>
2016-06-30 22:58:09 -0300

    Change code style.

--- test/controllers/test_squad_controller_test.rb
+++ test/controllers/test_squad_controller_test.rb
@@ -1,14 +1,14 @@
-require 'test_helper'
+require "test_helper"
 
 class TestSquadControllerTest < ActionController::TestCase
   def create_helper_file(framework)
-    File.open(@destination_root.join('test_squad.rb'), 'w') do |file|
+    File.open(@destination_root.join("test_squad.rb"), "w") do |file|
       file << "TestSquad.configuration.framework = '#{framework}'"
     end
   end
 
   setup do
-    @destination_root = Rails.root.join('test/javascript')
+    @destination_root = Rails.root.join("test/javascript")
     FileUtils.mkdir_p(@destination_root)
   end
 
@@ -16,35 +16,35 @@ class TestSquadControllerTest < ActionController::TestCase
     FileUtils.rm_rf @destination_root
   end
 
-  test 'view for mocha framework' do
-    create_helper_file 'mocha'
+  test "view for mocha framework" do
+    create_helper_file "mocha"
     get :tests
 
     assert_response :ok
-    assert_template 'mocha'
+    assert_template "mocha"
   end
 
-  test 'view for jasmine framework' do
-    create_helper_file 'jasmine'
+  test "view for jasmine framework" do
+    create_helper_file "jasmine"
     get :tests
 
     assert_response :ok
-    assert_template 'jasmine'
+    assert_template "jasmine"
   end
 
-  test 'view for qunit framework' do
-    create_helper_file 'qunit'
+  test "view for qunit framework" do
+    create_helper_file "qunit"
     get :tests
 
     assert_response :ok
-    assert_template 'qunit'
+    assert_template "qunit"
   end
 
-  test 'view for ember framework' do
-    create_helper_file 'ember'
+  test "view for ember framework" do
+    create_helper_file "ember"
     get :tests
 
     assert_response :ok
-    assert_template 'ember'
+    assert_template "ember"
   end
 end
